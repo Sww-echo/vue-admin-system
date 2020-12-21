d40fc8e5ec34b7907f74e0be00ade5010892c931
wwwali <walihyun@126.com>
2020-12-21 17:58:33 +0800

    基础表格2-wali

--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,72 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import systemHome from '../views/components/systemHome';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import systemHome from "../views/components/systemHome";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'systemHome',
+    path: "/",
+    name: "systemHome",
     component: systemHome
   },
   {
-    path: '/basicform',
-    name: 'basicForm ',
-    component: () => import('../views/components/basicForm')
+    path: "/basicform",
+    name: "basicForm ",
+    component: () => import("../views/components/basicForm")
   },
   {
-    path: '/basictable',
-    name: 'basicTable ',
-    component: () => import('../views/components/basicTable')
+    path: "/basictable",
+    name: "basicTable ",
+    component: () => import("../views/components/basicTable")
   },
   {
-    path: '/customicon',
-    name: 'customIcon ',
-    component: () => import('../views/components/customIcon')
+    path: "/customicon",
+    name: "customIcon ",
+    component: () => import("../views/components/customIcon")
   },
   {
-    path: '/droplist',
-    name: 'dropList ',
-    component: () => import('../views/components/dropList')
+    path: "/droplist",
+    name: "dropList ",
+    component: () => import("../views/components/dropList")
   },
   {
-    path: '/fileupload',
-    name: 'fileUpload ',
-    component: () => import('../views/components/fileUpload')
+    path: "/fileupload",
+    name: "fileUpload ",
+    component: () => import("../views/components/fileUpload")
   },
   {
-    path: '/internationalization',
-    name: 'internationalization ',
-    component: () => import('../views/components/internationalization')
+    path: "/internationalization",
+    name: "internationalization ",
+    component: () => import("../views/components/internationalization")
   },
   {
-    path: '/markdown',
-    name: 'markdown ',
-    component: () => import('../views/components/markdown')
+    path: "/markdown",
+    name: "markdown ",
+    component: () => import("../views/components/markdown")
   },
   {
-    path: '/popup',
-    name: 'popUp ',
-    component: () => import('../views/components/popUp')
+    path: "/popup",
+    name: "popUp ",
+    component: () => import("../views/components/popUp")
   },
   {
-    path: '/richtext',
-    name: 'richText ',
-    component: () => import('../views/components/richText')
+    path: "/richtext",
+    name: "richText ",
+    component: () => import("../views/components/richText")
   },
   {
-    path: '/schart',
-    name: 'schart ',
-    component: () => import('../views/components/schart')
+    path: "/schart",
+    name: "schart ",
+    component: () => import("../views/components/schart")
   },
   {
-    path: '/taboption',
-    name: 'tabOption ',
-    component: () => import('../views/components/tabOption')
+    path: "/taboption",
+    name: "tabOption ",
+    component: () => import("../views/components/tabOption")
   },
   {
-    path: '*',
+    path: "*",
     redirect: systemHome
   }
 ];
