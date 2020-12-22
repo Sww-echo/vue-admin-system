d0eade3083aaed047bc1e4cd3814607ceb8833c0
seeyouQian <875343519@qq.com>
2020-12-22 20:10:36 +0800

    国际化组件

--- src/views/components/internationalization.vue
+++ src/views/components/internationalization.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="internationalization page">
     <el-breadcrumb class="breadcrumb">
-      <el-breadcrumb-item><i class="el-icon-message message"></i>国际化产品</el-breadcrumb-item>
+      <el-breadcrumb-item><i class="el-icon-orange global"></i>国际化产品</el-breadcrumb-item>
     </el-breadcrumb>
-    <div class="internation-main">
+    <div class="internation-main main-wrap">
+      <el-button @click="changeLanguageEn">切换英文</el-button>
+      <el-button @click="changeLanguageCh">切换中文</el-button>
+      <p>{{$t('msg.info')}} <a href="https://element.eleme.cn/2.0/#/zh-CN/component/i18n">{{$t('msg.value')}}</a></p>
 
+      <el-carousel  type="card">
+        <el-carousel-item v-for=" (item , key) in $t('text') " v-bind:key="key">
+          <h3>{{item}}</h3>
+        </el-carousel-item>
+      </el-carousel>
     </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "internationalization"
+  name: "internationalization",
+  methods: {
+    changeLanguageEn: function(){
+      if(this.$i18n.locale !== 'en'){
+      this.$i18n.locale = 'en'
+      }
+    },
+    changeLanguageCh: function(){
+      if(this.$i18n.locale !== 'zh'){
+      this.$i18n.locale = 'zh'
+      }
+    },
+  }
 };
+
 </script>
 
-<style></style>
+<style lang='scss' >
+.internationalization{
+  box-sizing: border-box;
+    padding: 10px;
+}
+  .breadcrumb{
+    margin: 10px 0;
+  }
+  .main-wrap{
+    padding: 30px;
+    background: #fff;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+  }
+    .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    margin: 0;
+    display: flex;
+    height: 100%;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
---
+++ src/views/components/lang/i18n.js
@@ -0,0 +1,24 @@
+// 多语言设置
+// Element 兼容 vue-i18n@5.x
+import Vue from "vue"
+import VueI18n from 'vue-i18n'
+import enLocale from 'element-ui/lib/locale/lang/en'
+import zhLocal from 'element-ui/lib/locale/lang/zh-CN'
+import cnData from './cn'
+import enData from './en'
+const messages ={
+  en: {
+    ...enLocale,
+    ...enData
+  },
+  zh: {
+    ...zhLocal,
+    ...cnData
+  }
+}
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+  locale: 'zh',
+  messages
+})
+export default i18n
