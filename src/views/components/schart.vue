be384e131e4988e61bc077c09c1a5942a9fbdb4b
lcl-serendipity <1481807028@qq.com>
2020-12-23 19:20:11 +0800

    1

--- src/views/components/schart.vue
+++ src/views/components/schart.vue
@@ -84,10 +84,21 @@ export default {
             text:"服装品类销售饼状图",
             left:"center"
         },
-        series:[{
+        tooltip:{
+            trigger:"item"
+        },
+        series: [{
             type:"pie",
+            
             data:[{value:"334",name:"T恤"},{value:"278",name:"牛仔裤"},{value:"190",name:"连衣裙"},{value:"235",name:"毛衣"},{value:"260",name:"七分裤"},{value:"200",name:"短裙"},{value:"141",name:"羽绒服"}],
-            legendHoverLink: true //是否启用图例 hover 时的联动高亮
+            legendHoverLink: true, //是否启用图例 hover 时的联动高亮
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }    
         }],
         legend: {
             orient: "vertical", //图例列表的布局朝向horizontal  vertical
